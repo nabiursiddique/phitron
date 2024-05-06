8f1ea5fb211a79a7f7eb3e90d6141f975871c71e
nabiur siddique <nabiursiddique01@gmail.com>
2024-05-06 15:17:25 +0600

    Inserting element at the tail of a linked list

---
+++ Basic Data Structures/Module 06/insert_at_tail.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *next;
+
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, int val)
+{
+    // creating a new node
+    Node *newNode = new Node(val);
+
+    if (head == NULL)
+    {
+        head = newNode;
+        return;
+    }
+
+    Node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    // temp is now in last node
+
+    // Now we will connect our node into the last node
+    temp->next = newNode;
+}
+
+void print_linked_list(Node *head)
+{
+    cout << "Your linked list: ";
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *head = NULL;
+    while (true)
+    {
+        cout << "Option 1: Insert at tail" << endl;
+        cout << "Option 2: Print linked list" << endl;
+        cout << "Option 3: Terminate" << endl;
+        int op;
+        cin >> op;
+        if (op == 1)
+        {
+            cout << "Please enter value: ";
+            int value;
+            cin >> value;
+            insert_at_tail(head, value);
+        }
+        else if (op == 2)
+        {
+            print_linked_list(head);
+        }
+        else if (op == 3)
+        {
+            break;
+        }
+    }
+    return 0;
+}
